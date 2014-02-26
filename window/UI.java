8b7bb0c4010a77bfec577ed3c3954e7f451a93c5
Minijackson <minijackson@riseup.net>
2014-02-26 16:43:00 +0100

    Adding UIState enum + state UI field

--- window/UI.java
+++ window/UI.java
@@ -9,13 +9,9 @@ import javax.swing.JPanel;
 
 /**
  * Class used to handle the window's user interface
- * <p>
- * The UI is composed most of the time of two parts :
- * the top bar and the gameplay part (which basically
- * contain the world)
- * <p>
- * The UI can be also in "Loading mode" which consist
- * of a loading animation screen
+ * 
+ * This class can be in different states
+ * @see UIState
  */
 public class UI extends JPanel {
 
@@ -23,6 +19,12 @@ public class UI extends JPanel {
 	 * World displayed inside the window
 	 */
 	protected World displayedWorld;
+
+	/**
+	 * Current state of the Graphics displayed
+	 * @see UIState
+	 */
+	UIState state;
 
 	/**
 	 * Window height
@@ -35,7 +37,7 @@ public class UI extends JPanel {
 	protected int windowWidth;
 
 	/**
-	 * Height of the UI's topbar (default : 42)
+	 * Height of the UI's topbar (default : 41)
 	 */
 	protected static int TOPBAR_HEIGHT = 42;
 
@@ -53,7 +55,8 @@ public class UI extends JPanel {
 		this.windowHeight = windowHeight;
 		this.windowWidth = windowWidth;
 
-		displayedWorld = new World("test", windowHeight - TOPBAR_HEIGHT, windowWidth);
+		state = UIState.LoadingScreen;
+//		displayedWorld = new World("test", windowHeight - TOPBAR_HEIGHT, windowWidth);
 	}
 
 	/**
@@ -87,5 +90,47 @@ public class UI extends JPanel {
 		}
 
 	}
+
+};
+
+/**
+ * Enumeration of the possible states of the displayed graphics
+ * <p>
+ * The state can be the "in game" state, a loading screen,
+ * the "win" or "fail" screen
+ * @see UI
+ * @see #Game
+ * @see #LoadingScreen
+ * @see #Win
+ * @see #Fail
+ */
+enum UIState {
+	
+	/**
+	 * The Game state correspond to the state
+	 * in which the player is playing the game
+	 */
+	Game,
+
+	/**
+	 * The LoadingScreen state correspond to
+	 * the state where a loading animation
+	 * is displayed
+	 */
+	LoadingScreen,
+
+	/**
+	 * The Win state correspond to the state
+	 * where the player has won the game and
+	 * a congratulation screen is displayed
+	 */
+	Win,
+
+	/**
+	 * The Fail state correspond to the state
+	 * where the player has lost the game
+	 * and a condolence screen is displayed
+	 */
+	Fail
 
 };
