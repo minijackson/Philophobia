76d518eef5bb5c020fd600cd154a15047db6e377
Minijackson <minijackson@riseup.net>
2014-03-02 16:27:23 +0100

    Adding water and shore sprites + removing Water Shore styles

--- world/scenery/Water.java
+++ world/scenery/Water.java
@@ -10,44 +10,14 @@ import world.InteractiveObject;
 public class Water extends Scenery implements InteractiveObject {
 
 	/**
-	 * Temparate broadleaf water type
-	 */
-	protected static String TEMPERATE_BROADLEAF = "temperatebroadleaf";
-
-	/**
 	 * Taiga water type
 	 */
 	protected static String TAIGA = "taiga";
 
 	/**
-	 * Alpine water type
-	 */
-	protected static String ALPINE_TUNDRA = "alpinetundra";
-
-	/**
-	 * Steppe water type
-	 */
-	protected static String STEPPE = "steppe";
-
-	/**
-	 * Cave water type
-	 */
-	protected static String CAVE = "cave";
-
-	/**
 	 * Polar desert water type
 	 */
 	protected static String POLAR_DESERT = "polardesert";
-
-	/**
-	 * Xeric shrublands water type
-	 */
-	protected static String XERIC_SHRUBLANDS = "xericshrublands";
-
-	/**
-	 * Savanna water type
-	 */
-	protected static String SAVANNA = "savanna";
 
 	public Water(final String type) {
 
@@ -55,36 +25,12 @@ public class Water extends Scenery implements InteractiveObject {
 
 	}
 
-	public static String TEMPERATE_BROADLEAF() {
-		return TEMPERATE_BROADLEAF;
-	}
-
 	public static String TAIGA() {
 		return TAIGA;
 	}
 
-	public static String ALPINE_TUNDRA() {
-		return ALPINE_TUNDRA;
-	}
-
-	public static String STEPPE() {
-		return STEPPE;
-	}
-
-	public static String CAVE() {
-		return CAVE;
-	}
-
 	public static String POLAR_DESERT() {
 		return POLAR_DESERT;
-	}
-
-	public static String XERIC_SHRUBLANDS() {
-		return XERIC_SHRUBLANDS;
-	}
-
-	public static String SAVANNA() {
-		return SAVANNA;
 	}
 
 	// ===============================
